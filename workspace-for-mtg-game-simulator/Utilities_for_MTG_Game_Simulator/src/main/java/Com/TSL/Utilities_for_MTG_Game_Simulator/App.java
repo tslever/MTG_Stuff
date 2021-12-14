4a0d085f7dea4a72cb68bbb9fc1d1dbca515b2a5
tslever <thomas.lever.business@gmail.com>
2021-12-14 12:40:46 -0500

    Set up MTG Game Simulator
    As a Maven project.

---
+++ workspace-for-mtg-game-simulator/Utilities_for_MTG_Game_Simulator/src/main/java/Com/TSL/Utilities_for_MTG_Game_Simulator/App.java
@@ -0,0 +1,13 @@
+package Com.TSL.Utilities_for_MTG_Game_Simulator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
